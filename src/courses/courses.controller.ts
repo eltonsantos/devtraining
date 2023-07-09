f6f69ef6f407193ad0b6c44d7b86dc6adef0d451
Elton Santos <eltonaxl@hotmail.com>
2023-07-09 13:15:07 -0300

    Create entity

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
   @Get()
   findAll() {
     return 'Listagem de cursos';
