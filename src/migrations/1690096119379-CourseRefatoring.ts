582a8b0f923f8a5e225cab6f8735b9cdc68ee3c6
Elton Santos <eltonaxl@hotmail.com>
2023-07-25 13:31:17 -0300

    Create migrations

--- src/migrations/1690096119379-CourseRefatoring.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
-
-export class CourseRefatoring1690096119379 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
